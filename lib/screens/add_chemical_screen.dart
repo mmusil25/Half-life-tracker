91a51ec45ea0e6ed455d5180beca6ddcc430cfa2
Mark Musil <musilmark25@gmail.com>
2023-05-21 23:30:52 -0400

    save state working on form fields

--- lib/screens/add_chemical_screen.dart
+++ lib/screens/add_chemical_screen.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 
 
+class ChemicalInfo {
+  String? name = '';
+  num? timeZero = 0;
+  num? halfLife = 0;
+}
+
 class SecondRoute extends StatelessWidget{
   const SecondRoute({super.key});
 
+late FocusNode _chemName;
 
 @override
 Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
-      title: const Text('Second Route'),
+      title: const Text('Add chemical'),
     ),
-    body: Center(
-      child: ElevatedButton(
-        onPressed: () {
+    body: Column(
 
-          Navigator.pop(context);
+      children: [ 
+        
+        TextFormField(
+          restorationId: 'chemical_name_field',
+          focusNode: _chemName,
+          decoration: InputDecoration(
+            border:  OutlineInputBorder(),
+            hintText: "Chemical Name"
+            )
+          ),
 
-        },
-        child: const Text('Confirm addition'),
+
         ),
-      ),
+        
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Confirm addition'),
+        ),
+      ]
+    ),
+
+
     );
 }
 }
